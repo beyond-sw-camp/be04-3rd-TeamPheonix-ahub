95eee1de7691fdb09b8a5200a6770fdc2304d3fb
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-03-27 11:34:18 +0900

    registPage와 컴포넌트, loginPage와 컴포넌트 작성 완료
    registPage와 컴포넌트, loginPage와 컴포넌트 작성 완료

---
+++ components/00_member/loginInputForm.vue
@@ -0,0 +1,97 @@
+<template>
+    <form class="align">
+        <div class="input-form">
+            <label for="member_id">아이디</label><br>
+            <input class="input-style" type="text" id="member_id" name="member_id"><br>
+            <div class="span-margin">
+                <input type="checkbox" id="remember_id">
+                <span>아이디 저장</span>
+            </div>
+            <label for="member_id">비밀번호</label><br>
+            <input class="input-style" type="password" id="member_pwd" name="member_pwd"><br>
+            <div class="find-id-pwd-regist span-margin">
+                <span><a href="#">아이디 찾기</a>&nbsp;|</span>
+                <span><a href="#">비밀번호 찾기</a>&nbsp;| </span>
+                <span><a href="registPage.html">회원 가입</a> &nbsp;</span>
+            </div>
+        </div>
+        <button @click.prevent="tokenCheck">Login</button>
+    </form>
+</template>
+
+<script setup>
+function tokenCheck() {
+    if(member_id.value == ''){
+        alert('아이디를 입력해주세요.');
+        return false;
+    } else if (member_pwd.value == ''){
+        alert('비밀번호를 입력해주세요.');
+        return false;
+    } else {
+        // 서버 연동하여 토큰값 가져온 후 유효성 검사 코드 추가
+        console.log('로그인 성공');
+        return true;
+    }
+}
+</script>
+
+<style scoped>
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'GmarketSansMedium';
+}
+
+.align {
+    text-align: center;
+}
+
+.input-form {
+    width: 50vh;
+    margin: 0 auto;
+    text-align: left !important;
+}
+
+.input-style {
+    width: 50vh;
+    padding: 8px 14px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    background-color: #fefefe
+}
+
+input[type=checkbox] {
+    accent-color: #6D31ED;
+}
+
+.span-margin {
+    font-size: 12px;
+    margin-top: 5px;
+    margin-bottom: 1.5rem;
+}
+
+.find-id-pwd-regist {
+    text-align: right;
+}
+
+button {
+    background-color: #6D31ED;
+    color: white;
+    padding: 14px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+    font-style: bold;
+    width: 30vh;
+}
+</style>
---
+++ components/00_member/registInputForm.vue
@@ -0,0 +1,138 @@
+<template>
+    <form class="input-form align">
+        <div class="input-form-text">
+            <label>이름</label><br>
+            <input class="input-style required-text" type="text" ref="name" v-model.trim="member_name"><br>
+            <label>아이디</label><br>
+            <input class="input-style" type="text" ref="id" v-model.trim="member_id"><br>
+            <label>비밀번호</label><br>
+            <input class="input-style" type="password" ref="pwd" v-model.trim="member_pwd"><br>
+            <label>주소</label><br>
+            <input class="input-style" type="text" ref="addr" v-model="member_addr"><br>
+            <label>이메일</label><br>
+            <input class="input-style" type="text" ref="email" v-model.trim="member_email"><br>
+            <label>전화번호</label><br>
+            <input class="input-style" type="text" ref="phone" v-model.trim="member_phone"><br>
+        </div>
+        <div class="space">
+            <input type="checkbox" id="agree_policy">
+            <span>By signing up, I agree with the Terms of Use & Privacy Policy</span>
+        </div>
+        <div class="space">
+            <span @click="changeRouter">Already have an account? <a href="#">Log in</a></span>
+        </div>
+        <button @click.prevent="validateForm()">Sign up</button>
+    </form>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { useFocus } from '@vueuse/core';
+
+    const member_name = ref('');
+    const member_id = ref('');
+    const member_pwd = ref('');
+    const member_addr = ref('');
+    const member_email = ref('');
+    const member_phone = ref('');
+
+    function validateForm() {
+        var agreementCheckbox = document.getElementById("agree_policy");
+
+        if (member_name.value == '') {
+            alert('이름을 입력해주세요');
+            return false;
+        } else if(member_id.value == '') {
+            alert('아이디를 입력해주세요');
+            return false;
+        } else if (member_pwd.value == '') {
+            alert('비밀번호를 입력해주세요');
+            return false;
+        } else if (member_addr.value == '') {
+            alert('주소를 입력해주세요');
+            return false;
+        } else if (member_email.value == '') {
+            alert('이메일을 입력해주세요');
+            return false;
+        } else if (member_phone.value == '') {
+            alert('전화번호를 입력해주세요');
+            return false;
+        } else if (!agreementCheckbox.checked) {
+                alert("회원 가입 약관에 동의해주세요.");
+                return false;
+        } else {
+            console.log('회원 가입 완료')
+            return true;
+        }
+    }
+</script>
+
+<style scoped>
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'GmarketSansMedium';
+    font-size: 12px;
+}
+
+.align {
+    text-align: center;
+}
+
+.input-form {
+    width: 50vh;
+    margin: 0 auto;
+}
+
+.input-form-text {
+    text-align: left;
+
+}
+
+.required-text {
+    border: 2px solid red;
+}
+
+.input-style {
+    width: 50vh;
+    padding: 8px 14px;
+    margin: 3px 0 10px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    background-color: #fefefe
+}
+
+span {
+    font-size: 12px;
+}
+
+.space {
+    margin: 10px 0 10px 0;
+}
+
+input[type=checkbox] {
+    accent-color: #6D31ED;
+}
+
+button {
+    background-color: #6D31ED;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 12px;
+    font-style: bold;
+    width: 30vh;
+}
+</style>
